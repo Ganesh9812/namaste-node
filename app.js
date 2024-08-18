1b5b1a03aa75c1a10fa45c7bcd54fb1017219c8e
Ganesh Pavan <ganeshpavankumars@gmail.com>
2024-08-18 16:11:32 +0530

    episode 4

---
+++ app.js
@@ -0,0 +1,9 @@
+require("./xyz");
+const { x, calculateSum } = require("./sum");
+const data = require("./data.json");
+console.log(data, "ganesh");
+
+var a = 10;
+var b = 20;
+calculateSum(a, b);
+console.log(x);
---
+++ sum.js
@@ -0,0 +1,11 @@
+function calculateSum(a, b){
+    const sum = a + b;
+    console.log(sum);
+}
+
+const x = "hello"
+
+module.exports = {
+    x,
+    calculateSum
+};
