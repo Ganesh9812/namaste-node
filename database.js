883f8b180f66afbf8d2f6e204e0b97236ab318b4
Ganesh Pavan <ganeshpavankumars@gmail.com>
2024-08-26 16:51:28 +0530

    episode13

---
+++ database.js
@@ -0,0 +1,27 @@
+const { MongoClient } = require("mongodb");
+
+const url =
+  "mongodb+srv://ganeshpavankumars:cOeeng04sYTtEKA9@namastenode.yqrkb.mongodb.net/";
+
+const client = new MongoClient(url);
+
+const dbName = "Helloworld";
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  const collection = db.collection("user");
+
+  // the following code examples can be pasted here...
+
+  return "done.";
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
+//read from database
